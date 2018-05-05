75a57762338fad54ee65056b71ac4df87711296c
N1kko777 <3734489@gmail.com>
2018-05-05 22:46:20 +0500

    ###

--- js/script.js
+++ js/script.js
@@ -1,47 +1,104 @@
 window.addEventListener("DOMContentLoaded", function() {
   let navHamburger = document.getElementById("nav-hamburger"),
+    navDropdown = document.querySelector(".mobile-nav-dropdown"),
+    navMenu = document
+      .querySelector(".mobile-nav-dropdown-menu")
+      .getElementsByTagName("li"),
     sidebar = document.querySelectorAll(".sidebar-menu_item"),
+    basket = document.querySelector(".basket"),
     searchBlock = document.querySelector(".search-block"),
-    closeSearchBlock = document.querySelector(".search-block_form-input_close"),
-    url = window.location.origin;
+    searchForm = document.querySelector(".search-block_form"),
+    searchInput = document.querySelector(".search-block_form-input"),
+    searchPlaceholder = document.querySelector(
+      ".search-block_form-placeholder"
+    ),
+    closeBasket = document.querySelector(".basket_close"),
+    closeSearchBlock = document.querySelector(".search-block_form-input_close");
+  const URL = window.location.origin;
 
-    console.log(url);
-    
-
-  searchBlock.style.display = "none";
-
-  sidebar[2].addEventListener("click", event => {
-    document.body.classList.toggle("modal-open");
-    if (searchBlock.style.display == "none") {
-      searchBlock.style.display = "block";
-    } else {
-      searchBlock.style.transition = "all 0.5s ease";
-      searchBlock.style.opacity = "0";
-      setTimeout(() => {
-        searchBlock.style.display = "none";
-        searchBlock.style.opacity = "1";
-      }, 500);
-    }
+  /* go home :) */
+  sidebar[0].addEventListener("click", () => {
+    window.location.href = URL;
   });
 
-  navHamburger.addEventListener("click", () => {
-    document.body.classList.toggle("modal-open");
+  function scrollLock() {
+    if (document.body.classList.contains("modal-open")) {
+      document.body.classList.remove("modal-open");
+    } else {
+      document.body.classList.add("modal-open");
+    }
+  }
+
+  /* toggle any block with .d-n */
+  function toggleBlock(elem) {
+    scrollLock();
+    if (elem.classList.contains("d-n")) {
+      elem.classList.remove("d-n");
+    } else {
+      elem.classList.add("d-n");
+    }
+  }
+
+  /* toggle basket block */
+  sidebar[1].addEventListener("click", () => {
+    toggleBlock(basket);
+  });
+  closeBasket.addEventListener("click", () => {
+    toggleBlock(basket);
+  });
+
+  /* toggle search block */
+  sidebar[2].addEventListener("click", () => {
+    toggleBlock(searchBlock);
+  });
+  closeSearchBlock.addEventListener("click", () => {
+    toggleBlock(searchBlock);
+  });
+
+  /* search input action */
+  searchForm.addEventListener("click", event => {
+    let target = event.target;
+    if (
+      target.classList.contains("search-block_form-placeholder") ||
+      target.classList.contains("search-block_form-input")
+    ) {
+      searchPlaceholder.style.opacity = "0";
+      searchInput.focus();
+    }
+  });
+  searchInput.addEventListener("focus", () => {
+    searchPlaceholder.style.opacity = "0";
+    searchInput.focus();
+  }); /* focus */
+  searchInput.addEventListener("blur", () => {
+    if (searchInput.value === "") {
+      searchPlaceholder.style.opacity = "1";
+    }
+  }); /* blur */
+
+  /* toggle nav menu */
+  function toggleNavMenu() {
     for (var i = 0; i < navHamburger.getElementsByTagName("span").length; i++) {
       navHamburger.getElementsByTagName("span")[i].classList.toggle("close");
     }
+    navDropdown.classList.toggle("d-n");
+    scrollLock();
+  }
+  navHamburger.addEventListener("click", () => {
+    toggleNavMenu();
   });
 
-  closeSearchBlock.addEventListener("click", () => {
-    document.body.classList.toggle("modal-open");
-    if (searchBlock.style.display == "none") {
-      searchBlock.style.display = "block";
-    } else {
-      searchBlock.style.transition = "all 0.5s ease";
-      searchBlock.style.opacity = "0";
-      setTimeout(() => {
-        searchBlock.style.display = "none";
-        searchBlock.style.opacity = "1";
-      }, 500);
-    }
+  /* nav menu listner */
+  navMenu[0].addEventListener("click", () => {
+    toggleNavMenu();
+    window.location.href = URL;
+  });
+  navMenu[1].addEventListener("click", () => {
+    toggleNavMenu();
+    toggleBlock(basket);
+  });
+  navMenu[2].addEventListener("click", () => {
+    toggleNavMenu();
+    toggleBlock(searchBlock);
   });
 });
